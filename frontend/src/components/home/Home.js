2b3e807ec5735ebbf0003f46993822613a65b89b
Rajeev Ranjan <rjeevrnjn17@gmail.com>
2020-04-14 01:24:48 +0530

    added moment package and created late + ontime files

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import Late from "./Late";
+import Ontime from "./Ontime";
+import moment from "moment";
 
 class Home extends Component {
   render() {
     const { user } = this.props.auth;
     console.log(user);
     return (
+      /* rendering user data provided by the user */
       <div className='container-fluid mt-5'>
         <div className='container userdetail '>
           <p>Hi, {user.name}</p>
@@ -15,28 +19,14 @@ class Home extends Component {
           <p>Your Registered Mobile is : {user.mobile}</p>
           <p>and Your current department is :{user.department}</p>
         </div>
-
-        <div className='container markattendance mt-4'>
-          <div>
-            <p>
-              {" "}
-              <textarea
-                className='mt-4'
-                placeholder='Reason for being late today.. '
-                name='reason'
-                rows='2'
-                cols='30'></textarea>
-            </p>
-            <p>
-              {" "}
-              <button type='submit' className='btn btn-success markbtn mb-4'>
-                I'm Present
-              </button>{" "}
-            </p>
-            <br></br>
-            <p className='mt-5'> Successfully Submitted</p>
-          </div>
-        </div>
+        {/* conditionally rendering the I'm Present button */
+        /* rendering NORMAL
+        form between the time 9 am to 10 pm */}
+        {console.log(moment().format("HH:mm:ss"))}
+        <Ontime />
+        {/* rendering LATE form between the time 10:01 am to 5 pm */}
+        <Late />
+        {/*after 5 pm setting attendance to ABSENT by default */}
       </div>
     );
   }

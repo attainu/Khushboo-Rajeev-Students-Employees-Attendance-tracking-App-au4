b225a7e1623448233f0268fccb8b5b00603f5506
khushboo goyal <khushboogoyal27@gmail.com>
2020-04-14 19:51:10 +0530

    deleted Late/Ontime

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -2,35 +2,72 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
-import Late from "./Late";
-import Ontime from "./Ontime";
-import moment from "moment";
 
 class Home extends Component {
+
+
+  state = {
+    status: false,
+    click: false
+
+  };
+  handleAttendance = () => {
+
+    this.setState({
+
+      status: true,
+      click: true
+    }, () => {
+      console.log("clicked state", this.state);
+    })
+  }
+
+
   render() {
     const { user } = this.props.auth;
-    console.log(user);
+    console.log("user", user);
 
     return (
-      /* rendering user data provided by the user */
       <div className='container-fluid mt-5'>
         <div className='container userdetail '>
+
           <div>
-            <p className='mt-5'>Hi, {user.name}</p>
-            <p>Joining Date : {user.joined}</p>
-            <p>Registered Email :{user.email}</p>
-            <p>Registered Mobile : {user.mobile}</p>
-            <p>Department :{user.department}</p>
+            <p className="mt-5">Hi, {user.name}</p>
+            <p>Your Joining Date is : {user.joined}</p>
+            <p>Your Registered Email is :{user.email}</p>
+            <p>Your Registered Mobile is : {user.mobile}</p>
+            <p>and Your current department is :{user.department}</p>
+          </div>
+
+        </div>
+
+        <div className='container markattendance mt-4'>
+          <div>
+            <p>
+              {" "}
+              <textarea
+                className='mt-4'
+                placeholder='Reason for being late today.. '
+                name='reason'
+                rows='2'
+                cols='30'></textarea>
+            </p>
+            <p>
+              {" "}
+              <button type='submit' className='btn btn-success markbtn mb-4' type='submit'
+                disabled={this.state.click === false ? "" : "disable"}
+                onClick={() => {
+                  this.handleAttendance();
+                }} >
+                I'm Present
+              </button>{" "}
+
+            </p>
+            <br></br>
+            {}
+            <p className='mt-5' hidden={this.state.status && this.state.click === true ? "" : "hidden"}> Successfully Submitted</p>
           </div>
         </div>
-        {/* conditionally rendering the I'm Present button */
-        /* rendering NORMAL
-        form between the time 9 am to 10 pm */}
-        {console.log(moment().format("HH:mm:ss"))}
-        <Ontime />
-        {/* rendering LATE form between the time 10:01 am to 5 pm */}
-        <Late />
-        {/*after 5 pm setting attendance to ABSENT by default */}
       </div>
     );
   }
